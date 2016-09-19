fe20aa55eb1856b1bc2b5cf4857b1e23c3f428e3
kotesto <kotesto@ukr.net>
2016-09-19 19:00:29 +0300

    Commin 5.5

--- src/module05/DAOImpl.java
+++ src/module05/DAOImpl.java
@@ -3,19 +3,19 @@ package module05;
 public class DAOImpl implements DAO{
     @Override
     public Room save(Room room) {
-        System.out.println(room + " saved to DB.");
+        System.out.println(room.toString() + " saved to DB.");
         return room;
     }
 
     @Override
     public boolean delete(Room room) {
-        System.out.println(room + " delete from DB.");
+        System.out.println(room.toString() + " delete from DB.");
         return false;
     }
 
     @Override
     public Room update(Room room) {
-        System.out.println(room + " update DB.");
+        System.out.println(room.toString() + " update DB.");
         return room;
     }
 
