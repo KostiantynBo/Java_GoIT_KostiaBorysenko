5784af5ffb25b5ff6dee9bebcbfd68403aca6398
kotesto <kotesto@ukr.net>
2016-10-10 18:27:21 +0300

    7.1

---
+++ src/module07/Order.java
@@ -0,0 +1,13 @@
+package module07;
+
+import java.util.Currency;
+
+public class Order {
+    private long id;
+    private int price;
+    private Currency currency;
+    private String itemName;
+    private String shopIdentificator;
+    private User user;
+
+}
---
+++ src/module07/User.java
@@ -0,0 +1,11 @@
+package module07;
+
+
+public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private int balance;
+}
