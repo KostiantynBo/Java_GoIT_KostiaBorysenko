40bcfa215348ebd16faaca2f32d0aa21b7694fe7
kotesto <kotesto@ukr.net>
2016-11-07 15:08:55 +0200

    11 without refactoring

--- src/module11/Main111.java
+++ src/module11/Main111.java
@@ -5,42 +5,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main111 {
-        public static void main(String[] args) throws IOException {
-            Map<String, String> map = new HashMap<>();
-            map.put("love", "hate");
-            map.put("you", "YOU");
-            //read file
-            BufferedReader br;
-            try {
-                FileReader fileReader = new FileReader("/Users/KotestoMac/Desktop/K.txt");
-                br = new BufferedReader(fileReader);
-            } catch (FileNotFoundException e) {
-                System.out.println("file not found");
-                return;
-            }
-
-            // save to string var
-
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-
-            while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
-                line = br.readLine();
-            }
-            br.close();
-
-
-            String result = sb.toString();
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                String key = entry.getKey();
-                String value = entry.getValue();
-                result = result.replaceAll(key, value);
-            }
+    public static void main(String[] args) throws IOException {
+        Map<String, String> map = new HashMap<>();
+        map.put("love", "hate");
+        map.put("you", "YOU");
+        //read file
+        BufferedReader br;
+        try {
+            FileReader fileReader = new FileReader("/Users/KotestoMac/Desktop/K.txt");
+            br = new BufferedReader(fileReader);
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found");
+            return;
         }
 
+        // save to string var
 
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
+
+        while (line != null) {
+            sb.append(line);
+            sb.append(System.lineSeparator());
+            line = br.readLine();
+        }
+        br.close();
+
+
+        String result = sb.toString();
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            result = result.replaceAll(key, value);
+        }
+    }
 }
-
-
